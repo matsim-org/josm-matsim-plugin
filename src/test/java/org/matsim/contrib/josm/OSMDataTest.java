eb758eda83db33abcedb0b3c7a4926478b018199
Nico <nkuehnel2@gmail.com>
2015-02-21 01:40:21 +0100

    INPROGRESS test for osm data

--- src/test/java/org/matsim/contrib/josm/OSMDataTest.java
+++ src/test/java/org/matsim/contrib/josm/OSMDataTest.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Assert;
@@ -11,7 +13,15 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
 import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.data.osm.WaySegment;
 import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import org.openstreetmap.josm.io.Compression;
 import org.openstreetmap.josm.io.IllegalDataException;
@@ -23,27 +33,68 @@ public class OSMDataTest {
 	
 	    @Rule
 	    public TemporaryFolder folder = new TemporaryFolder();
+	    
+	    private OsmDataLayer incompleteWayLayer;
+	    private OsmDataLayer busRouteLayer;
+	    private NetworkListener busRouteListener;
 
 	    @Before
 	    public void init() {
 	        new JOSMFixture(folder.getRoot().getPath()).init(false);
+	        URL urlIncompleteWay = getClass().getResource("/test-input/OSMData/incompleteWay.osm");
+	        URL urlRoute = getClass().getResource("/test-input/OSMData/busRoute.osm");
+		       
+	        InputStream incompleteWayInput = null;
+	        InputStream busRouteInput = null; 
+			try {
+				incompleteWayInput = Compression.getUncompressedFileInputStream(new File(urlIncompleteWay.getFile()));
+				busRouteInput = Compression.getUncompressedFileInputStream(new File(urlRoute.getFile()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	DataSet incompleteWayData = null;
+	    	DataSet busRouteData = null;
+			try {
+				incompleteWayData = OsmReader.parseDataSet(incompleteWayInput, null);
+				busRouteData = OsmReader.parseDataSet(busRouteInput, null);
+			} catch (IllegalDataException e) {
+				e.printStackTrace();
+			}
+	    	incompleteWayLayer = new OsmDataLayer(incompleteWayData, "test", null);
+	    	busRouteLayer = new OsmDataLayer(busRouteData, "test", null);
+	    	busRouteListener = new NetworkListener(busRouteData, ScenarioUtils.createScenario(ConfigUtils.createConfig()), new HashMap<Way, List<Link>>(), new HashMap<Link, List<WaySegment>>(), new HashMap<Relation, TransitRoute>());
+            busRouteListener.visitAll();
+	    
 	    }
 
 	    @Test
-	    public void readOsmFile() throws InterruptedException, ExecutionException, IOException, IllegalDataException {
-	        URL url = getClass().getResource("/test-input/OSMData/incompleteWay.osm");
-	        loadIncompleteWay(new File(url.getFile()));
+	    public void testIncompleteWay() throws InterruptedException, ExecutionException, IOException, IllegalDataException {
+	    	  LayerConverter converter =  new LayerConverter(incompleteWayLayer);
+		        converter.run();
+	    	Assert.assertEquals(0,converter.getMatsimLayer().data.getWays().size());
+	        Assert.assertEquals(0,converter.getMatsimLayer().getScenario().getNetwork().getLinks().size());
+	        
+	    }
+	    
+	    @Test
+	    public void testBusRoute() throws InterruptedException, ExecutionException, IOException, IllegalDataException {
+		        Assert.assertEquals(18,busRouteListener.getScenario().getNetwork().getLinks().size());
+	            Assert.assertEquals(10,busRouteListener.getScenario().getNetwork().getNodes().size());
+	            Assert.assertEquals(4,busRouteListener.getScenario().getTransitSchedule().getFacilities().size());
+	            Assert.assertEquals(1,busRouteListener.getScenario().getTransitSchedule().getTransitLines().size());
+	            Assert.assertEquals(2,busRouteListener.getScenario().getTransitSchedule().getTransitLines().get(Id.create("-116", TransitRoute.class)).getRoutes().size());
+		    	for (TransitRoute route: busRouteListener.getScenario().getTransitSchedule().getTransitLines().get(Id.create("-116", TransitRoute.class)).getRoutes().values()) {
+		    		Assert.assertEquals(4, route.getStops().size());
+		    		Assert.assertEquals(9, route.getRoute().getLinkIds().size());
+		    	}
+		    	LayerConverter converter =  new LayerConverter(busRouteLayer);
+		    	converter.run();
+	        
 	    }
 
-	    private void loadIncompleteWay(File file) throws InterruptedException, ExecutionException, IOException, IllegalDataException {
-	    	InputStream in = Compression.getUncompressedFileInputStream(file);
-	    	DataSet data = OsmReader.parseDataSet(in, null);
-	    	OsmDataLayer layer = new OsmDataLayer(data, "test", file);
-	        LayerConverter converter =  new LayerConverter(layer);
-	        converter.run();
-	        Assert.assertEquals(0,converter.getMatsimLayer().data.getWays().size());
-	        Assert.assertEquals(0,converter.getMatsimLayer().getScenario().getNetwork().getLinks().size());
-	    }
+	    
+	    
+	 
 
 	   
 
