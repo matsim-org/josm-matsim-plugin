5347dbb73d9aa3ec745076557475fb023fd7116b
Michael Zilske <michael.zilske@tu-berlin.de>
2015-08-25 11:52:35 +0200

    oops, simply creating a link doesn't work either. :-(

--- src/test/java/org/matsim/contrib/josm/InteractiveEditingTest.java
+++ src/test/java/org/matsim/contrib/josm/InteractiveEditingTest.java
@@ -35,15 +35,36 @@ public class InteractiveEditingTest {
 		Way way = new Way();
 		way.addNode(node1);
 		way.addNode(node2);
-		way.getKeys().put("freespeed", "10.0");
-		way.getKeys().put("capacity", "1000.0");
-		way.getKeys().put("permlanes", "1.0");
-		way.getKeys().put("modes", "car");
+		way.put("freespeed", "10.0");
+		way.put("capacity", "1000.0");
+		way.put("permlanes", "1.0");
+		way.put("modes", "car");
 		new AddCommand(matsimLayer, way).executeCommand();
 		Assert.assertEquals(1, matsimLayer.getScenario().getNetwork().getLinks().size());
+	}
+
+	@Test
+	public void createLinkDeleteUndoDelete() {
+		MATSimLayer matsimLayer = NewNetworkAction.createMatsimLayer();
+		Main.main.addLayer(matsimLayer);
+		Node node1 = new Node();
+		node1.setCoor(new LatLon(0.0, 0.0));
+		new AddCommand(matsimLayer, node1).executeCommand();
+		Node node2 = new Node();
+		node2.setCoor(new LatLon(0.1, 0.1));
+		new AddCommand(matsimLayer, node2).executeCommand();
+		Way way = new Way();
+		way.addNode(node1);
+		way.addNode(node2);
+		way.put("freespeed", "10.0");
+		way.put("capacity", "1000.0");
+		way.put("permlanes", "1.0");
+		way.put("modes", "car");
+		new AddCommand(matsimLayer, way).executeCommand();
 		DeleteCommand delete = new DeleteCommand(way);
 		delete.executeCommand();
-		Assert.assertEquals(0, matsimLayer.getScenario().getNetwork().getLinks().size());
+		delete.undoCommand();
+		Assert.assertEquals(1, matsimLayer.getScenario().getNetwork().getLinks().size());
 	}
 
 }
