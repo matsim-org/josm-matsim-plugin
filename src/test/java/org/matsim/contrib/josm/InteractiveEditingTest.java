a6134b6fd715d67775f52c0356feac42bb40126e
Michael Zilske <michael.zilske@tu-berlin.de>
2015-08-25 11:44:43 +0200

    Add a test for #26

--- src/test/java/org/matsim/contrib/josm/InteractiveEditingTest.java
+++ src/test/java/org/matsim/contrib/josm/InteractiveEditingTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.command.AddCommand;
+import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.DeleteCommand;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.Node;
@@ -44,6 +45,29 @@ public class InteractiveEditingTest {
 		DeleteCommand delete = new DeleteCommand(way);
 		delete.executeCommand();
 		Assert.assertEquals(0, matsimLayer.getScenario().getNetwork().getLinks().size());
+	}
+
+
+	@Test
+	public void createLinkThenSetMatsimAttribtues() {
+		MATSimLayer matsimLayer = NewNetworkAction.createMatsimLayer();
+		Main.main.addLayer(matsimLayer);
+		Node node1 = new Node();
+		node1.setCoor(new LatLon(0.0, 0.0));
+		new AddCommand(matsimLayer, node1).executeCommand();
+		Node node2 = new Node();
+		node2.setCoor(new LatLon(0.1, 0.1));
+		new AddCommand(matsimLayer, node2).executeCommand();
+		Way way = new Way();
+		way.addNode(node1);
+		way.addNode(node2);
+		new AddCommand(matsimLayer, way).executeCommand();
+		Assert.assertEquals(0, matsimLayer.getScenario().getNetwork().getLinks().size());
+		new ChangePropertyCommand(way, "freespeed", "10.0").executeCommand();
+		new ChangePropertyCommand(way, "capacity", "1000.0").executeCommand();
+		new ChangePropertyCommand(way, "permlanes", "1.0").executeCommand();
+		new ChangePropertyCommand(way, "modes", "car").executeCommand();
+		Assert.assertEquals(1, matsimLayer.getScenario().getNetwork().getLinks().size());
 	}
 
 }
