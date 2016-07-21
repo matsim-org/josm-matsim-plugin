69d431021b0a63ec8dc5cac8cac91a78dec6f41a
Michael Zilske <michael.zilske@tu-berlin.de>
2016-07-21 15:59:11 +0200

    Fix build.gradle

---
+++ src/main/java/org/matsim/contrib/gtfs/LineBrowser.java
@@ -0,0 +1,16 @@
+package org.matsim.contrib.gtfs;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LineBrowser extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+}
